188d09b24c1b49e12b7c7c944d2fd6056a4ccfe3
eaccid <elena.briginets@gmail.com>
2024-01-17 13:22:17 +0000

    try with livedata in repository

--- app/src/main/java/com/eaccid/musimpa/entities/Authentication.kt
+++ app/src/main/java/com/eaccid/musimpa/entities/Authentication.kt
@@ -4,7 +4,9 @@ data class Authentication(
     val success: Boolean?,
     val expires_at: String?,
     val request_token: String?,
-    val session_id: String?
+    val session_id: String?,
+    val status_code: Int?,
+    val status_message: String?
 ) {
-    constructor(request_token: String) : this(null, null, request_token, null)
+    constructor(request_token: String) : this(null, null, request_token, null, null, null)
 }
--- app/src/main/java/com/eaccid/musimpa/repository/AuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/eaccid/musimpa/repository/AuthenticationRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.eaccid.musimpa.repository
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.eaccid.musimpa.BuildConfig
 import com.eaccid.musimpa.LocalPreferences
 import com.eaccid.musimpa.entities.Authentication
@@ -10,17 +12,19 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class AuthenticationRepositoryImpl(
-    private val serviceAPI: TMDbServiceAPI,
-    private val preferences: LocalPreferences
+class AuthenticationRepositoryImpl(private val serviceAPI: TMDbServiceAPI, private val preferences: LocalPreferences
 ) :
     AuthenticationRepository {
 
-    //TODO handle responstatus
+    //TODO handle responses and status mvvm properly
 
-    override fun login() {
+    private val _isSucceed = MutableLiveData<Boolean>()
+    val isSucceed: LiveData<Boolean> get() = _isSucceed
+
+    override fun login(){
         val params = mapOf("api_key" to BuildConfig.THE_MOVIE_DB_API_KEY)
         val authenticationCall: Call<Authentication> = serviceAPI.requestToken(API_VERSION, params)
+        _isSucceed.value = false
         authenticationCall.enqueue(object : Callback<Authentication> {
             override fun onResponse(
                 call: Call<Authentication>,
@@ -33,13 +37,17 @@ class AuthenticationRepositoryImpl(
                     LocalPreferences.SharedKeys.TOKEN.key,
                     auth?.request_token ?: EMPTY_STRING_VALUE
                 )
-//                if (success) redirectToAuthenticateWebView(
-                auth?.request_token ?: EMPTY_STRING_VALUE
-//                )
+                if (success) {
+//                    redirectToAuthenticateWebView
+                    _isSucceed.value = true
+                    auth?.request_token ?: EMPTY_STRING_VALUE
+                }
             }
 
             override fun onFailure(call: Call<Authentication>, t: Throwable) {
+                Log.i("Authorization", "Failed")
                 preferences.saveString(LocalPreferences.SharedKeys.TOKEN.key, EMPTY_STRING_VALUE)
+                _isSucceed.value = false
             }
         })
     }
