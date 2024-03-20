f06ba1d34d5f922752b9a644370b1777c876e96a
eaccid <elena.briginets@gmail.com>
2024-03-20 15:50:35 +0000

    added progressview to movie details screen

--- app/src/main/java/com/eaccid/musimpa/ui/moviedetails/MovieDetailsScreen.kt
+++ app/src/main/java/com/eaccid/musimpa/ui/moviedetails/MovieDetailsScreen.kt
@@ -2,8 +2,11 @@ package com.eaccid.musimpa.ui.moviedetails
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -19,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.eaccid.musimpa.ui.customview.ProgressView
 import com.eaccid.musimpa.ui.theme.MusimpaTheme
 import com.eaccid.musimpa.ui.uientities.MovieItem
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
@@ -41,36 +46,74 @@ fun MoviesDetailsScreenContent(
 ) {
     if (viewState is MovieDetailsScreenViewState.Success) {
         val dataItem: MovieItem = viewState.movie
-        Column(modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth()) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
             YoutubeScreen(
                 dataItem.videoKey,
                 Modifier
                     .fillMaxWidth()
             )
-            Column(
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(8.dp)
+                    .padding(0.dp, 16.dp, 0.dp, 0.dp)
             ) {
-                Text(
-                    text = dataItem.title ?: "Non",
-                    style = TextStyle(
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.DarkGray
-                    )
-                )
-                Text(
-                    text = dataItem.releaseDate ?: "Non",
+                AndroidView(modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp), factory = {
+                    val view = ProgressView(it).apply {
+
+                    }
+                    view.progress = dataItem.voteAverage?.times(10)?.toInt() ?: 0
+                    view.setTextSize(24)
+                    view
+                })
+                Column(
                     modifier = Modifier
-                        .padding(0.dp, 8.dp, 0.dp, 0.dp)
-                )
-                Text(
-                    text = dataItem.voteAverage.toString() ?: "Non"
-                )
+                        .fillMaxWidth()
+                        .padding(16.dp, 0.dp, 0.dp, 0.dp)
+                ) {
+                    Text(
+                        text = dataItem.title ?: "Non",
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.DarkGray
+                        )
+                    )
+                    Text(
+                        modifier = Modifier
+                            .padding(0.dp, 0.dp, 0.dp, 8.dp),
+                        style = TextStyle(
+                            fontStyle = FontStyle.Italic
+                        ),
+                        text = dataItem.tagline ?: "Non"
+                    )
+                    Text(
+                        text = dataItem.releaseDate ?: "Non",
+                    )
+                    Text(
+                        text = "${dataItem.runtime.toString()} min"
+                    )
+                }
             }
+            Text(
+                modifier = Modifier
+                    .padding(0.dp, 16.dp, 0.dp, 0.dp),
+                text = "Overview",
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    color = Color.DarkGray
+                )
+            )
+            Text(
+                text = dataItem.overview ?: "Non",
+                modifier = Modifier
+                    .padding(0.dp, 4.dp, 0.dp, 0.dp)
+            )
         }
     } else {
         Text(text = "todo no data error handling")
